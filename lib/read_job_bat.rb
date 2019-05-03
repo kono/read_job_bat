8d951aff0e26c0ce37d7a115adae90f837ed9b9f
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-05-04 08:22:49 +0900

    Remove initialize method because it is empty

--- lib/read_job_bat.rb
+++ lib/read_job_bat.rb
@@ -3,8 +3,6 @@ require "read_job_bat/version"
 module ReadJobBat
   class Error < StandardError; end
   class ReadJobBat
-    def initialize
-    end
 
     def get_fullpath_bat(drive, dir, bat)
       unless bat.match("\:")
@@ -23,7 +21,7 @@ module ReadJobBat
       ar=[]
       drive=dir=""
       s.split("\n").each do |line| 
-        drive = line if line =~/[A-Za-z]\:/ and line.length==2
+        drive = line if line =~/[A-Za-z]\:/ and line.strip.length==2
         dir = line.split("\s")[1] if line.downcase =~ /^cd /
         if line.downcase =~ /^call/
           ar.push get_fullpath_bat(drive, dir, line.split("\s")[1])
