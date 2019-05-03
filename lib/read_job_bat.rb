016b96059cf4a9f105b2189786409a08c056c0ba
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-05-03 14:54:28 +0900

    Implement simple core procedure

--- lib/read_job_bat.rb
+++ lib/read_job_bat.rb
@@ -2,5 +2,27 @@ require "read_job_bat/version"
 
 module ReadJobBat
   class Error < StandardError; end
-  # Your code goes here...
+  class ReadJobBat
+    def initialize
+    end
+    def readbat(s)
+      ar=[]
+      drive=dir=""
+      s.split("\n").each do |line| 
+        drive = line if line =~/[A-Za-z]\:/
+        dir = line.split("\s")[1] if line.downcase =~ /^cd /
+        p dir
+        if line.downcase =~ /^call/
+          if drive=="" and dir==""
+            dirname="\.\\"
+          else
+            dirname=drive + dir + "\\"
+          end
+          ar.push dirname + line.split("\s")[1]
+        end
+      end
+      p ar
+      ar
+    end
+  end 
 end
--- test/read_job_bat_test.rb
+++ test/read_job_bat_test.rb
@@ -5,7 +5,28 @@ class ReadJobBatTest < Test::Unit::TestCase
     refute_nil ::ReadJobBat::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_can_read_bat_strings
+    target = ReadJobBat::ReadJobBat.new
+    input =<<~EOS
+    E:
+    cd \\TEST\\bat
+    call foobar.bat
+    EOS
+    assert target.readbat(input) == ['E:\TEST\bat\foobar.bat']
+    input =<<~EOS
+    E:
+    cd \\TEST2\\bat
+    call bazqux.bat
+    EOS
+    assert target.readbat(input) == ['E:\TEST2\bat\bazqux.bat']
+  end
+
+  def test_that_it_can_read_bat_files
+    target = ReadJobBat::ReadJobBat.new
+    f = File.open('test/simple_testbat.txt')
+    s=f.read
+    f.close
+    assert target.readbat(s) == ['E:\TEST\bat\foobar.bat']
+    
   end
 end
